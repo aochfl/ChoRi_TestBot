9b60d501d6a64f0bb5d210bf8c5b17f6af822b22
Jinyeong Jeung <aochfl_@naver.com>
2022-09-07 14:35:16 +0900

    embeds 꾸미기 추가

--- main.py
+++ main.py
@@ -21,7 +21,16 @@ async def ping(ctx):
 
 @app.command(name="핑")
 async def ping(ctx):
-    embed = discord.Embed(title="ping pong", description="핑퐁")
-    await ctx.reply('퐁', embeds=[embed, embed])
+    embed = discord.Embed(title="ping pong", description="핑퐁", colour=0xffffff)
+    embed.set_author(name='Author name', icon_url="https://han.gl/XBMeC")
+    embed.set_footer(text="footer", icon_url="https://han.gl/XBMeC")
+    embed.set_image(url="https://han.gl/XBMeC")
+    embed.set_thumbnail(url="https://han.gl/XBMeC")
+    embed.add_field(name="필드1", value="inline false 필드1 내용", inline=False)
+    embed.add_field(name="필드2", value="inline false 필드2 내용", inline=False)
+    embed.add_field(name="필드3", value="inline True 필드3 내용", inline=True)
+    embed.add_field(name="필드4", value="inline True 필드4 내용", inline=True)
+    await ctx.reply('퐁', embed=embed)
+
 
 asyncio.run(main())
