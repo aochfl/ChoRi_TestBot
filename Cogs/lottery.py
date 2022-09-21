0eb32bea314b9a01ad8e5f321cb22c5717f9f9b0
Jinyeong Jeung <aochfl_@naver.com>
2022-09-21 12:00:44 +0900

    모든 로또 추첨번호 다운로드 기능 추가

--- Cogs/lottery.py
+++ Cogs/lottery.py
@@ -1,3 +1,4 @@
+import os
 import discord
 from discord.ext import commands
 from bs4 import BeautifulSoup
@@ -6,10 +7,11 @@ import urllib.request
 
 class LotteryFunction:
     def __init__(self):
-        self.lastGameCount = None  # 1033회
+        self.lastGameCount = None  # 1033
         self.lastGameDate = None  # (2022년 09월 17일 추첨)
         self.lastGameNumbers = []
         self.parse_newest()
+        self.listFilePath = f'./lottery{self.lastGameCount}.xls'
 
     # 현재 회차 정보 파싱
     def parse_newest(self):
@@ -22,6 +24,17 @@ class LotteryFunction:
         numberList = soup.select('div.win_result > div > .num > p .ball_645')
         for number in numberList:
             self.lastGameNumbers.append(number.get_text())
+
+    # 모든 추첨 번호 다운 로드
+    def download_total(self):
+        if os.path.isfile(self.listFilePath):
+            print("lottery list exist")
+        else:
+            self.delete_list('*[0-9].xls')
+            start_count = 1
+            url = f'https://dhlottery.co.kr/gameResult.do?method=allWinExel&gubun=byWin&nowPage=1&' \
+                  f'drwNoStart={start_count}&drwNoEnd={self.lastGameCount}'
+            urllib.request.urlretrieve(url, self.listFilePath)
 
 
 class Lottery(commands.Cog):
@@ -39,9 +52,16 @@ class Lottery(commands.Cog):
         # 데이터로부터 회차 확인
         embed.add_field(name="회차", value=self.lottery.lastGameCount+"회", inline=True)
         embed.add_field(name="추첨번호", value=self.lottery.lastGameNumbers, inline=False)
-
         await ctx.send(embed=embed)
-        
+
+    @commands.command(name="다운")
+    async def download_list(self, ctx):
+        msg = await ctx.send("다운로드 시작")
+        self.lottery.download_total()
+        await ctx.message.delete()
+        await msg.delete()
+        await ctx.send("다운로드 완료", delete_after=5)
+
 
 async def setup(app):
     await app.add_cog(Lottery(app))
