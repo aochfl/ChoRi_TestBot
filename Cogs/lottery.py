3b4f2fb5059017186782e74b2d50347f373eca07
Jinyeong Jeung <aochfl_@naver.com>
2022-09-23 16:29:19 +0900

    다운받은 추첨번호 리스트 가공 추가

--- Cogs/lottery.py
+++ Cogs/lottery.py
@@ -1,4 +1,5 @@
 import os
+import glob
 import discord
 from discord.ext import commands
 from bs4 import BeautifulSoup
@@ -11,6 +12,7 @@ class LotteryFunction:
         self.lastGameDate = None  # (2022년 09월 17일 추첨)
         self.lastGameNumbers = []
         self.parse_newest()
+        self.mCounterList = {}  # 로또 숫자 별 추첨 횟수
         self.listFilePath = f'./lottery{self.lastGameCount}.xls'
 
     # 현재 회차 정보 파싱
@@ -25,6 +27,11 @@ class LotteryFunction:
         for number in numberList:
             self.lastGameNumbers.append(number.get_text())
 
+    def delete_list(self, filename: str):
+        for name in glob.glob(filename):
+            print(name + "deleted")
+            os.remove(name)
+
     # 모든 추첨 번호 다운 로드
     def download_total(self):
         if os.path.isfile(self.listFilePath):
@@ -35,6 +42,25 @@ class LotteryFunction:
             url = f'https://dhlottery.co.kr/gameResult.do?method=allWinExel&gubun=byWin&nowPage=1&' \
                   f'drwNoStart={start_count}&drwNoEnd={self.lastGameCount}'
             urllib.request.urlretrieve(url, self.listFilePath)
+
+    # 다운받은 리스트를 통해 계산
+    def calculate_list(self):
+        if os.path.isfile(self.listFilePath):
+            for idx in range(1, 46):
+                self.mCounterList[str(idx)] = 0  # 리스트 초기화
+            default_row = 3   # 로또 list 에서 추첨 번호가 시작 하는 행
+            list_xml = open(self.listFilePath, encoding='cp949')  # 저장된 list 불러오기
+            soup = BeautifulSoup(list_xml, 'html.parser')  # 데이터 파싱
+            total_info = soup.findAll('tr')
+            for row in range(default_row, len(total_info)):
+                row_data = total_info[row].findAll('td')
+                max_col = len(row_data)
+                for col in range(max_col-7, max_col):
+                    # 각 숫자가 뽑힌 횟수 계산
+                    self.mCounterList[row_data[col].text] += 1
+        else:
+            self.download_total()
+            self.calculate_list()
 
 
 class Lottery(commands.Cog):
@@ -61,6 +87,15 @@ class Lottery(commands.Cog):
         await ctx.message.delete()
         await msg.delete()
         await ctx.send("다운로드 완료", delete_after=5)
+
+    @commands.command(name="계산")
+    async def calc_list(self, ctx):
+        msg = await ctx.send("계산시작")
+        self.lottery.calculate_list()
+        await msg.delete()
+        embed = discord.Embed(title="로또 당첨번호 내역", description="번호별 당첨 횟수를 출력합니다")
+        embed.add_field(name="내역", value=self.lottery.mCounterList, inline=True)
+        await ctx.send("계산완료", embed=embed)
 
 
 async def setup(app):
